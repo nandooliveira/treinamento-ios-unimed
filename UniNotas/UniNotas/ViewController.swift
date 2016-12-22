2e989ee12c9e04a0a9560fb5174f0c3f3d53468a
Fernando Oliveira <nandooliveira.al@gmail.com>
2016-12-22 20:26:52 -0300

    inicio projeto notas

---
+++ UniNotas/UniNotas/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  UniNotas
+//
+//  Created by Fernando Oliveira on 22/12/16.
+//  Copyright © 2016 Unimed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func salvarNota(_ sender: Any) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
