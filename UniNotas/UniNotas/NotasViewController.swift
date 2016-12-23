a907a88ffcf892b52126fe6b1daf588a3f098efc
Fernando Oliveira <nandooliveira.al@gmail.com>
2016-12-22 21:43:40 -0300

    notas

---
+++ UniNotas/UniNotas/NotasViewController.swift
@@ -0,0 +1,52 @@
+//
+//  NotasViewController.swift
+//  UniNotas
+//
+//  Created by Fernando Oliveira on 22/12/16.
+//  Copyright © 2016 Unimed. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class NotasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let context = delegate.persistentContainer.viewContext
+        
+        let busca = NSFetchRequest<NSFetchRequestResult>(entityName: "Nota")
+        
+        do {
+            let notas = context.fetch(busca)
+        } catch {
+            print("Deu erro")
+        }
+        
+        for nota in notas {
+            
+            print(nota.)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UniNotas/UniNotas/ViewController.swift
+++ UniNotas/UniNotas/ViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textView: UIView!
+    @IBOutlet weak var textView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let notaAtual = UserDefaults.standard.object(forKey: "nota")
+        
+        if notaAtual != nil {
+            textView.text = notaAtual as! String
+        }
+        
     }
 
     @IBAction func salvarNota(_ sender: Any) {
+        UserDefaults.standard.set(textView.text, forKey: "nota")
         
+        let alert = UIAlertController(title: "Sucesso!", message: "Nota Salva!", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { (action) in
+            self.textView.resignFirstResponder()
+        })
+        
+        present(alert, animated: true, completion: nil)
+        self.textView.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
